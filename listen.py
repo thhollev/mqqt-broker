1eef8a06ee649ddf640d1f403172981a405e80f1
thhollev <thhollev.hollevoet@ugent.be>
2017-02-10 11:22:48 +0100

    major update

--- listen.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-while True:
-    dat = open('temp.dat')
-    print(dat.read())
-    dat.close()
--- send.py
+++ send.py
@@ -4,9 +4,10 @@ import paho.mqtt.publish as publish
 import sys
 
 def POST():
-	if len(sys.argv) != 3:
+	publish.single(str(sys.argv[2]), str(sys.argv[3]), hostname=str(sys.argv[1]))
+
+if __name__ == '__main__':
+	if len(sys.argv) != 4:
 		print("Usage: "+sys.argv[0]+" <ip> <topic> <message>")
 	else:
-		publish.single(str(sys.argv[2], str(sys.argv[3]), hostname=str(sys.argv[1]))
-
-POST()
+		POST()
