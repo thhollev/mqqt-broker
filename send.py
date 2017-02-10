aaf8b61e4b0428f95682a90bd631597d15dcbd42
thhollev <thhollev.hollevoet@ugent.be>
2017-02-10 10:18:56 +0100

    update send

--- send.py
+++ send.py
@@ -5,8 +5,8 @@ import sys
 
 def POST():
 	if len(sys.argv) != 3:
-		print("Usage: "+sys.argv[0]+" <ip> <message>")
+		print("Usage: "+sys.argv[0]+" <ip> <topic> <message>")
 	else:
-		publish.single("temp", str(sys.argv[2]), hostname=str(sys.argv[1]))
+		publish.single(str(sys.argv[2], str(sys.argv[3]), hostname=str(sys.argv[1]))
 
 POST()
